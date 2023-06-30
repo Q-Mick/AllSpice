d0853cc22bb4078306e418c1e707472bfd7410d1
Q-Mick <quentinmick@protonmail.com>
2023-06-30 14:56:39 -0600

    added favorites files

---
+++ AllSpice/Repositories/FavoritesRepository.cs
@@ -0,0 +1,25 @@
+namespace AllSpice.Repositories;
+
+public class FavoritesRepository
+{
+  private readonly IDbConnection _db;
+
+  public FavoritesRepository(IDbConnection db)
+  {
+    _db = db;
+  }
+
+  internal Favorite CreateFavorite(Favorite favoriteData)
+  {
+    string sql = @"
+    INSERT INTO favorites
+    (accountId, recipeId)
+    VALUES
+    (@accountId, @recipeId);
+    SELECT LAST_INSERT_ID();
+    ";
+    int lastInsertId = _db.ExecuteScalar<int>(sql, favoriteData);
+    favoriteData.Id = lastInsertId;
+    return favoriteData;
+  }
+}
